ebafb627e1f471140f49a0c611e1a300bf7a9263
AIfrk <109033934+AIfrk@users.noreply.github.com>
2020-02-22 13:38:39 +0000

    remove logo

--- Backend/globals.py
+++ Backend/globals.py
@@ -10,4 +10,21 @@ UPLOAD_FOLDER = ABSOLUTE_PATH+'/uploads'
 ALLOWED_EXTENSIONS = set(['npz', 'csv', 'tsv', 'pkl', 'pickle', 'txt'])
 
 def allowed_file(filename):
-	return '.' in filename 
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# Used in selecting module name in sklearn.
+# Can be replaced
+
+PREPROC = ['preprocessing', 'impute']
+SUPER = ['ensemble', 'gaussian_process', 'kernel_ridge', 'linear_model', 'neighbours', 'svm' ,'tree']
+UNSUPER = ['cluster', 'decomposition']
+
+# Used for Wrapping and Unwrapping data for communication
+
+json_decoder = json.JSONDecoder()
+json_encoder = json.JSONEncoder()
+
+# Used to create weak references to dictionary
+
+class weakdict(dict):
