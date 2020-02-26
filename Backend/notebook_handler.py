3d3d66de30b758e99e020deeccb0676c8e62503a
AIfrk <109033934+AIfrk@users.noreply.github.com>
2020-02-26 18:01:57 +0000

    Bump for dev.

--- Backend/notebook_handler.py
+++ Backend/notebook_handler.py
@@ -11,4 +11,24 @@ def create_notebook_global_table():
 
 	return True
 
-# check whether the global notebook 
+# check whether the global notebook table exists
+
+def notebook_global_table_exist():
+	return os.path.exists("NOTEBOOKS_DATA")	
+
+# Global variable to keep track of opened notebooks
+
+ACTIVE_NOTEBOOKS = {}
+
+# Returns a reference to notebook data
+
+def get_notebook_data(notebook_name):
+
+	# if notebook has not been loaded yet
+	if notebook_name not in ACTIVE_NOTEBOOKS:
+
+		# load as append binary and reading
+		fileObject = open("NOTEBOOK_" + notebook_name, "ab+")
+		
+		# pickle needs the file pointer to point to starting of the file
+		fileObject
