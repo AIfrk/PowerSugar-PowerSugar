0eeea8dc768dcae13d80d1c56427ca092004a546
AIfrk <109033934+AIfrk@users.noreply.github.com>
2022-09-24 22:46:56 +0000

    Removed unnecessary comments.

--- src/components/InvestigateModel.vue
+++ src/components/InvestigateModel.vue
@@ -7,4 +7,25 @@
     <br>
     <b-container  class="rounded text-center" >
       <img :src="original_image">
-      <img :src="construct
+      <img :src="constructed_image">
+      <b-embed type="iframe" id="explanation_embed" aspect="16by9" allowfullscreen :src="explanation_embed"></b-embed>
+    </b-container>
+
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    feather.replace()
+    this.loadNotebook()
+  },
+  data: function() {
+      return {
+        original_image: null,
+        constructed_image: null,
+        explanation_embed: null
+      }
+  },
+  methods: {
+      // Called on body load
+      // Checks if notebook exists and populates UI if noteb
