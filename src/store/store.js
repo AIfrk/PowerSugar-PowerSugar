22d950ad47d6a04291cd8cf12309d2787b51f74c
AIfrk <109033934+AIfrk@users.noreply.github.com>
2023-01-29 15:27:31 +0000

    more cosmetic fixes

--- src/store/store.js
+++ src/store/store.js
@@ -6,4 +6,7 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
 
   state : {
-      logged
+      loggedUser : null,
+      nonNeuralModelSelected: "LinearSVC",
+      currentNotebook: '',
+      sklearnJSON : {"calibration": {}, "cluster": {"AffinityPropagation": {"__init__": [["self", "damping", "max_iter", "convergence_iter", "copy", "preference", "affinity", "verbose"], [0.5, 200, 15, "True", "None", "euclidean", "False"]]}, "AgglomerativeClustering": {"__init__": [["self", "n_clusters", "affinity", "memory", "connectivity", "compute_full_tree", "linkage", "pooling_func"], [2, "euclidean", "None
