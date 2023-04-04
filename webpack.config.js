6ed54c080106aa7f2b7c6d5c7e0c4e1f8b99b5c2
AIfrk <109033934+AIfrk@users.noreply.github.com>
2023-04-04 07:03:59 +0000

    @@

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,28 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    pat
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_mod
